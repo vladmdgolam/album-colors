01648162c6b04793a858d97f5fed90301f3fb619
vladmdgolam <vladmdgolam@ya.ru>
2020-12-19 00:49:46 +0300

    style: fancy update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,18 @@
 import Head from "next/head"
+import styled from "styled-components"
 
 import CardList from "@/components/cardList"
+
+const Credits = styled.div`
+  padding: 20px;
+`
+const P = styled.p`
+margin-bottom: 5px;
+`
+
+const Link = styled.a`
+  text-decoration: underline;
+`
 
 export default function Home() {
   return (
@@ -10,17 +22,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <CardList />
-      <p>
-        Источники: плейлист Николая Редькина{" "}
-        <a href="https://open.spotify.com/playlist/2ntwTz2ctoV4etEqFK6vNH?si=kvMz23fMTVeGgLzfMKtvLQ">
-          «2020: крутая русская музыка»
-        </a>
-        , итоги года Тихого места и The-Flow, лента друзей Spotify, итоги года
-        Яндекс.музыки
-      </p>
-      <p>
-        Придумал и разработал: <a href="https://vladmdgolam.now.sh/">Влад</a>
-      </p>
+      <Credits>
+        <P>
+          Источники: плейлист Николая Редькина{" "}
+          <Link href="https://open.spotify.com/playlist/2ntwTz2ctoV4etEqFK6vNH?si=kvMz23fMTVeGgLzfMKtvLQ">
+            «2020: крутая русская музыка»
+          </Link>{" "}
+          и Александра Горбачёва{" "}
+          <Link href="https://open.spotify.com/playlist/2RpRwEHD33Yjs3yq4KiAIc?si=vAne_VGQTcyroJz9tqmX6g">
+            «2020»
+          </Link>
+          , итоги года{" "}
+          <Link href="https://the-flow.ru/megaitogi2020">The-Flow</Link> и{" "}
+          <Link href="https://music.yandex.ru/users/music-blog/playlists/2459">
+            Яндекс.Музыки
+          </Link>{" "}
+          и лента друзей Spotify
+        </P>
+        <P>
+          Придумал и разработал:{" "}
+          <Link href="https://vladmdgolam.now.sh/">Влад Мд</Link>, дизайн:{" "}
+          <Link href="https://www.instagram.com/921d0/">Катя До</Link>
+        </P>
+      </Credits>
     </div>
   )
 }
---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: {
+    "postcss-import": {},
+    // "postcss-nesting": {},
+    // "postcss-simple-vars": {},
+    // "postcss-flexbugs-fixes": {},
+    // "postcss-preset-env": {
+    //   autoprefixer: {
+    //     flexbox: "no-2009",
+    //   },
+    //   stage: 3,
+    //   features: {
+    //     "custom-properties": false,
+    //   },
+    // },
+  },
+}
