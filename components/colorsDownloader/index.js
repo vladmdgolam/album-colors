7e74e91103cb82ba31339b7d6cb18dfbaab8297a
vladmdgolam <vladmdgolam@ya.ru>
2020-12-25 16:50:44 +0300

    content: resort coors

--- components/colorsDownloader/index.js
+++ components/colorsDownloader/index.js
@@ -1,10 +1,57 @@
-import { useRef, useState, useEffect } from "react"
+import { useRef, useState, useEffect, useContext } from "react"
 
 import sortColorsByHue from "@/lib/sortColors"
 import ColorWork from "./colorWork"
 import data from "@/data/albums.json"
+import { ReactSortable } from "react-sortablejs"
+import AppContext from "@/hooks/AppContext"
+import getFileName from "@/lib/getFilename"
+
+import styled from "styled-components"
 
 import Mix from "./mix"
+
+const Img = styled.img`
+  will-change: opacity;
+  transition: var(--transition);
+  opacity: ${({ showColors }) => (showColors ? 0 : 1)};
+  width: 100%;
+`
+
+const ColorEl = styled.div`
+  background: ${({ color }) => color};
+  height: 100px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const Flex = styled(ReactSortable)`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(10%, 1fr));
+`
+
+const Sort = (props) => {
+  const [state, setState] = useState(data)
+  const { showColors, setShowColors } = useContext(AppContext)
+
+  return (
+    <>
+      <button onClick={() => setShowColors(!showColors)}>toggle colors</button>
+      <button onClick={() => download(state)}>download sorted</button>
+      <Flex list={state} setList={setState}>
+        {state.map((item) => {
+          const filename = getFileName(item.artist, item.album)
+          return (
+            <ColorEl showColors={showColors} color={item.color} key={item.id}>
+              <Img showColors={showColors} src={filename} />
+            </ColorEl>
+          )
+        })}
+      </Flex>
+    </>
+  )
+}
 
 const ColorsDownloader = () => {
   const [albums, setAlbums] = useState(data)
@@ -51,6 +98,7 @@ const ColorsDownloader = () => {
         download not mixed
       </button>
       <Mix />
+      <Sort />
       {colorCompute &&
         data.map((song, key) => (
           <ColorWork
